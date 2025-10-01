6523d66c101d5c9846a777508b0d854de6c9de76
Scryptolog1st <jgibbs.online@gmail.com>
2025-10-01 19:59:00 -0400

    chore(web): configure Vite React app (vite.config, tsconfig, index.html, main.tsx)

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,6 +1,10 @@
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-import { App } from './ui/App'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { App } from './ui/App';
 
-const root = createRoot(document.getElementById('root')!)
-root.render(<App />)
+const root = document.getElementById('root')!;
+ReactDOM.createRoot(root).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
